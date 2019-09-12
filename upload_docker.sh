94b5eff8e751b5512a4b9a5208427928ca8feb9f
Brian Gilbank <Brian_Gilbank@intuit.com>
2019-09-12 15:27:19 -0600

    modified run_docker.sh and upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="bgilbank/api"
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+# echo "Docker ID and Image: $dockerpath"
+docker login -u bgilbank &&\ docker tag v1 api:version1.0
+
 
 # Step 3:
 # Push image to a docker repository
+docker push api:v1
