78e9fbfcd5efa5eaec28bd29157e58f020e36e76
Brian Gilbank <Brian_Gilbank@intuit.com>
2019-09-18 12:34:26 -0600

    Completed step 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="bgilbank/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run bgilbank-api --image=$dockerpath:v1 --port=8080 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/api 8000:80
